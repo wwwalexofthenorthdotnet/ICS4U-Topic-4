a6eda38e1b78ebda1a30f00b54dd79c0e4292ea5
Alexofthenorth <alexofthenorth10@protonmail.com>
2026-09-10 12:13:10 -0400

    Add project files.

---
+++ ICS4U Topic 4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICS4U_Topic_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomIntegers();
+        }
+
+        public static void RandomIntegers()
+        {
+            Random generator = new Random();
+            int maxValue = 0;
+            int minValue = 0;
+
+            bool maxValueValid = false;
+            bool minValueValid = false;
+
+            // Title
+
+            Console.WriteLine($"Random Integer");
+            Console.WriteLine();
+
+            // Enter Values
+
+            while (!maxValueValid)
+            {
+                Console.WriteLine("Enter the maximum value: ");
+               
+
+                if (!Int32.TryParse(Console.ReadLine(), out maxValue))
+                {
+                    Console.WriteLine("Invalid input.");
+                    continue;
+                }
+                else
+                {
+
+                
+                
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ ICS4U Topic 4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ICS4U Topic 4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TVDSB")]
+[assembly: AssemblyProduct("ICS4U Topic 4")]
+[assembly: AssemblyCopyright("Copyright © TVDSB 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fcf1a8e0-7521-424e-9ada-ff7d192d36a6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
